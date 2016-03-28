2f8d3900e9d3f117415e0399dadcd0c90e45d4e8
Hunter Bennett <HkB115@users.noreply.github.com>
2016-03-28 14:54:39 -0500

    Update adderRippleCarry16bit.vhd

--- adderRippleCarry16bit.vhd
+++ adderRippleCarry16bit.vhd
@@ -10,27 +10,29 @@ library work;
 use work.def.all;
 
 entity adderRippleCarry16bit is    
-port(
- A_in : in std_logic_vector(15 downto 0);
- B_in : in std_logic_vector(15 downto 0);
- Cin : in std_logic;
- S : out std_logic_vector(15 downto 0);
- Cout : out std_logic);
-end adderRippleCarry16bit;
+  port
+    (
+    A_in : in std_logic_vector(15 downto 0);
+    B_in : in std_logic_vector(15 downto 0);
+    Cin : in std_logic;
+    S : out std_logic_vector(15 downto 0);
+    Cout : out std_logic
+    );
+  end adderRippleCarry16bit;
 
-architecture struct of adderRippleCarry16bit is
-signal im  :    std_logic_vector(14 downto 0);
-signal imi :    std_logic_vector(14 downto 0);
-begin
- c0   : fulladd 
-        port map (A_in(0),B_in(0),Cin,S(0),im(0));
- c01  : carry 
-        port map (im(0),imi(0));
- c    : for i in 1 to 14 generate
-         c1to14:  fulladd port map (A_in(i),B_in(i),
-         imi(i-1),S(i),im(i));
-         c27to32: carry port map (im(i),imi(i));
-        end generate;
- c15   : fulladd port map (A_in(15),B_in(1),
-         imi(14),S(15),Cout);
-end struct;
+architecture structure of adderRippleCarry16bit is
+  signal im : std_logic_vector(14 downto 0);
+  signal imi : std_logic_vector(14 downto 0);
+  begin
+    c0 : fulladd
+      port map (A_in(0),B_in(0),Cin,S(0),im(0));
+    c01 : carry
+      port map (im(0),imi(0));
+    c : for i in 1 to 14 generate
+      c1to14:  fulladd port map (A_in(i),B_in(i),
+      imi(i-1),S(i),im(i));
+      c27to32: carry port map (im(i),imi(i));
+      end generate;
+   c15 : fulladd port map (A_in(15),B_in(1),
+      imi(14),S(15),Cout);
+  end structure;
