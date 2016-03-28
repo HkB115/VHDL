3dd4c6e201625c07a9200a18d4491dd99fe3e0c0
Hunter Bennett <HkB115@users.noreply.github.com>
2016-03-28 14:50:41 -0500

    Update fulladd.vhd

--- fulladd.vhd
+++ fulladd.vhd
@@ -4,16 +4,18 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity fulladd is
-port(
- A : in std_logic;
- B : in std_logic;
- Cin : in std_logic;
- S : out std_logic;
- Cout : out std_logic);
-end fulladd;
+  port
+    (
+    A : in std_logic;
+    B : in std_logic;
+    Cin : in std_logic;
+    S : out std_logic;
+    Cout : out std_logic
+    );
+  end fulladd;
 
-architecture Behavioral of fulladd is
-begin
- S <= A xor B xor Cin;
- Cout <= (A and B) or (Cin and (A or B));
-end Behavioral;
+architecture behavior of fulladd is
+  begin
+    S <= A xor B xor Cin;
+    Cout <= (A and B) or (Cin and (A or B));
+  end behavior;
