bea4e374264aa598fa985595ae3150e9f20598b7
Hunter Bennett <HkB115@users.noreply.github.com>
2016-04-11 14:08:14 -0500

    Create MooreFiniteStateMachine.vhd

---
+++ MooreFiniteStateMachine.vhd
@@ -0,0 +1,51 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity MooreFiniteStateMachine is
+  port
+    (
+    Clock: in std_logic;
+    Resetn: in std_logic;
+    W: in std_logic;
+    Z : out std_logic
+    );
+  end MooreFiniteStateMachine;
+
+architecture behavior of MooreFiniteStateMachine is
+type State_type is (A, B, C);
+signal y: State_type;
+  begin
+  process(Resetn, Clock)
+    begin
+    if(Resetn = '0')
+      then
+      y <= A;
+    elsif(Clock'EVENT and Clock = '1')
+      then
+      case y is
+        when A =>
+          if(W = '0')
+            then
+            y <= A;
+          else
+            y <= B;
+          end if;
+        when B =>
+          if(W = '0')
+            then
+            y <= A;
+          else
+            y <= C;
+          end if;
+        when C =>
+          if(W = '0')
+            then
+            y <= A;
+          else
+            y <= C;
+          end if;
+      end case;
+    end if;
+    end process;
+  Z <= '1' when y = C else '0';
+  end behavior;
